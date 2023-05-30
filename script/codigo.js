cccc27ed1a32b2dbe8dcf00ef85de722c48daa65
Emanuel Dominguez <Emanueldominguez@live.com.ar>
2023-05-30 18:47:04 -0300

    Se agrego form contacto

---
+++ script/codigo.js
@@ -0,0 +1,12 @@
+let nombre;
+let apellido;
+let edad;
+
+nombre = "Emanuel";
+apellido = "dominguez";
+edad = "32";
+
+console.log("Hola soy "+nombre " "+apellido+ "y mi edad es " +edad);
+
+
+//alert("Hola Me llamo"+nombre+apellido "y tengo" +cantidad+mascota)//
