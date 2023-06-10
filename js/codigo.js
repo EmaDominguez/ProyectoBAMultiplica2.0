1dc6754504f6b3858890d2e26cd5905ce6203fcb
Emanuel Dominguez <Emanueldominguez@live.com.ar>
2023-06-10 13:05:10 -0300

    Se edito js

--- js/codigo.js
+++ js/codigo.js
@@ -1,11 +1,17 @@
-// Muestra el año actual, para actualizarse atuomaticamente
-var currentYear = new Date().getFullYear();
-document.getElementById("current-year").textContent = currentYear;
-
-
-document.getElementById("myFooter").addEventListener("click", miFuncion);
-
-function miFuncion() {
-// Mensaje Emergente
-  alert("Felicitaciones! Encontraste el panel secreto. Mencionando 'POROTOS QUEMADOS' obtendras un 20% de descuento en nuestros servicios, que lo disfrutes! ");
+// Condicionales
+if (document.getElementById("myFooter")) {
 }
+
+// Variable
+var currentYear = new Date().getFullYear();
+
+// Funcion
+function mostrarMensaje() {
+  alert("Felicitaciones! Encontraste el panel secreto. Mencionando 'POROTOS QUEMADOS' obtendrás un 20% de descuento en nuestros servicios, que lo disfrutes!");
+}
+
+// Métodos
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("current-year").textContent = currentYear;
+  document.getElementById("myFooter").addEventListener("click", mostrarMensaje);
+});
